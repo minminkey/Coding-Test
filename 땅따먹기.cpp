89e1aaced10d714666a1dbb524e61d9d2d8e5ced
alsrl8259 <alsrl8259@gmail.com>
2020-12-26 15:06:43 +0900

    upload

---
+++ 땅따먹기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(vector<vector<int> > land)
+{
+    int answer = 0;
+    int s = land.size()-1;
+    for(int i=0; i<s; i++){
+        land[i+1][0] += max(land[i][1], max(land[i][3], land[i][2]));
+        land[i+1][1] += max(land[i][0], max(land[i][3], land[i][2]));
+        land[i+1][2] += max(land[i][0], max(land[i][1], land[i][3]));
+        land[i+1][3] += max(land[i][0], max(land[i][1], land[i][2]));
+    }
+    answer = max(land[s][0], max(land[s][1], max(land[s][2], land[s][3])));
+    return answer;
+}
