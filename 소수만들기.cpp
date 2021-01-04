b8646f6604b0f06f4773052e448b5b2d1dc2ac80
alsrl8259 <alsrl8259@gmail.com>
2021-01-04 13:52:09 +0900

    Summer/Winter Codint(~2018)

---
+++ 소수만들기.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+vector<int> candidate;
+int cnt;
+int visit[51];
+
+bool find(int x){
+    for(int i=2; i<=sqrt(x); i++){
+        if(x%i == 0)    return false;
+    }
+    return true;
+}
+void dfs(int n, int start, vector<int> nums){
+    if(n>=3){
+        int sum = 0;
+        for(int i=0; i<3; i++){
+            sum += candidate[i];
+        }
+        if(find(sum))   cnt++;
+        return;
+    }
+    for(int i=start; i<nums.size(); i++){
+        if(visit[i] == 1)   continue;
+        visit[i] = 1;
+        candidate.push_back(nums[i]);
+        dfs(n+1, i+1, nums);
+        candidate.pop_back();
+        visit[i] = 0;
+    }
+}
+int solution(vector<int> nums) {
+    int answer = -1;
+    dfs(0, 0, nums);
+    answer = cnt;
+    return answer;
+}
