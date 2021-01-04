be6857f4644789fc412b2c44f851922051487a06
alsrl8259 <alsrl8259@gmail.com>
2021-01-04 16:54:46 +0900

    2017 카카오코드 본선

--- 단체 사진.cpp
+++ 단체 사진.cpp
@@ -1,33 +1,48 @@
-bool isRight(char c, int diff, int num){
-    if(c == '='){
-        return diff == num;
-    }else if(c == '>'){
-        return diff > num;
-    }else if(c == '<'){
-        return diff < num;
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool visit[8];
+char kakao[8] = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+int CNT = 0;
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+void dfs(int cnt, char arr[], vector<string> data){
+    if(cnt==8){
+        for(int i=0; i<data.size(); i++){
+            char tmp1 = data[i][0];
+            char tmp2 = data[i][2];
+            char cond = data[i][3];
+            int count = data[i][4] - '0';
+            count++;
+            int idx1 = -1;
+            int idx2 = -1;
+            for(int j=0; j<8; j++){
+                if(arr[j]==tmp1)    idx1 = j;
+                if(arr[j]==tmp2)    idx2 = j;
+                if(idx1!=-1&&idx2!=-1)  break;
+            }
+            if(cond=='='&&abs(idx1-idx2)!=count)    return;
+            if(cond=='>'&&abs(idx1-idx2)<=count)    return;
+            if(cond=='<'&&abs(idx1-idx2)>=count)    return;
+        }
+        CNT++;
+        return;
+    }
+    for(int i=0; i<8; i++){
+        if(visit[i]==true)  continue;
+        visit[i]=true;
+        arr[cnt] = kakao[i];
+        dfs(cnt+1, arr, data);
+        visit[i]=false;
     }
 }
 
 int solution(int n, vector<string> data) {
     int answer = 0;
-    string friends = "ACFJMNRT";
-    
-    do{
-        bool flag = true;
-        for(string text : data){
-            char ff = text[0];
-            char sf = text[2];
-            char comp = text[3];
-            int num = text[4] - '0';
-            int diff = friends.find(ff) - friends.find(sf);
-			int dis = abs(diff) - 1;
-            
-            if(isRight(comp, dis, num)) continue;
-            flag = false;
-            break;
-        }
-        if(flag) answer++;
-    }while(next_permutation(friends.begin(), friends.end()));
-    
+    CNT = 0;
+    char arr[8] = {NULL, };
+    dfs(0, arr, data);
+    answer = CNT;
     return answer;
 }
