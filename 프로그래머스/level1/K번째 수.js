0e43b17a9a202dd8eade60357cb470b24e8d00b7
alsrl8259 <alsrl8259@gmail.com>
2021-06-10 16:22:50 +0900

    Sort

--- 프로그래머스/level1/K번째 수.js
+++ 프로그래머스/level1/K번째 수.js
@@ -5,11 +5,4 @@ function solution(array, commands) {
         answer.push(tmp[com[2]-1]);
     })
     return answer;
-    function solution(array, commands) {
-        var answer = [];
-        commands.forEach(com => {
-            let tmp = array.slice(com[0]-1, com[1]).sort((a,b) => a-b);
-            answer.push(tmp[com[2]-1]);
-        })
-        return answer;
-    }
+}
